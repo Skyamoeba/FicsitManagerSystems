b8010e538221204c3d53a85aade854b7d76af248
UKLoneWolf <halljason@live.co.uk>
2022-05-29 21:50:41 +0100

    Test Ended

--- Ver.lua
+++ Ver.lua
@@ -74,7 +74,4 @@ function VerCheckPowerD()
 function VerCheckPowerP()
   return "1.0.0"
   end
-function KnownIssuesPower()
-write(2,31,"If Consuption == 0 then displays Power production below usage ########  FIX NEEDED / REWRITE")
-end
 -- End Power Manager Version Checker
